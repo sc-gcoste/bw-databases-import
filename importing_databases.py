98754fa1dc0480ba14705f1deae50858c75cb2df
Gustave Coste <gustave.coste@sustain-cert.com>
2022-12-02 08:56:47 +0100

    Moved from sc-iftm-ef-calculation

---
+++ importing_databases.py
@@ -0,0 +1,143 @@
+import brightway2 as bw
+
+from custom_import_migrations import wfldb_technosphere_migration_data, agb_technosphere_migration_data
+
+
+def add_simapro_categories(importer):
+    for process in importer:
+        if 'exchanges' in process:
+            products = [x for x in process['exchanges'] if x['type'] == 'production']
+            assert len(products) == 1
+            product = products[0]
+            process['simapro_categories'] = product['categories']
+
+
+def main():
+    bw.projects.set_current('EF calculation')
+    bw.bw2setup()
+
+    # Ecoinvent 3.9
+    # db_name = 'ecoinvent3.9_cutoff'
+    # # del bw.databases[db_name]
+    # if db_name in bw.databases:
+    #     print("Database has already been imported.")
+    # else:
+    #     fp_ei = rf"../databases/ecoinvent 3.9_cutoff_ecoSpold02/datasets"
+    #     ei = bw.SingleOutputEcospold2Importer(fp_ei, db_name, use_mp=False)
+    #     ei.apply_strategies()
+    #     ei.statistics()
+    #
+    #     # Changing the process names to differentiate coproducts
+    #     for activity in ei:
+    #         if activity['name'] not in \
+    #                 (activity['reference product'], "market for " + activity['reference product']):
+    #             activity['name'] = f"{activity['name']} - {activity['reference product']}"
+    #
+    #     ei.write_database()
+
+    # Ecoinvent 3.8
+    db_name = 'ecoinvent3.8_cutoff'
+    # del bw.databases[db_name]
+    if db_name in bw.databases:
+        print("Database has already been imported.")
+    else:
+        fp_ei = r"../databases/ecoinvent 3.8_cutoff_ecoSpold02/datasets"
+        ei = bw.SingleOutputEcospold2Importer(fp_ei, db_name, use_mp=False)
+        ei.apply_strategies()
+        ei.statistics()
+
+        # Changing the process names to differentiate coproducts
+        for activity in ei:
+            if activity['name'] not in \
+                    (activity['reference product'], "market for " + activity['reference product']):
+                activity['name'] = f"{activity['name']} - {activity['reference product']}"
+
+        ei.write_database()
+
+    # Agribalyse
+    # del bw.databases['agribalyse3']
+    if 'agribalyse3' in bw.databases:
+        print("Database has already been imported.")
+    else:
+        agb_csv_filepath = r"../databases/agribalyse3_no_param.CSV"
+
+        agb_importer = bw.SimaProCSVImporter(agb_csv_filepath, "agribalyse3")
+
+        agb_technosphere_migration = bw.Migration("agb-technosphere")
+        agb_technosphere_migration.write(
+            agb_technosphere_migration_data,
+            description="Specific technosphere fixes for Agribalyse 3"
+        )
+
+        agb_importer.apply_strategies()
+        agb_importer.statistics()
+        agb_importer.migrate("agb-technosphere")
+        agb_importer.match_database("ecoinvent3.8_cutoff", fields=('reference product', 'location', 'unit', 'name'))
+        agb_importer.apply_strategies()
+        agb_importer.statistics()
+
+        # The only remaining unlinked exchanges are final waste flows and a land use change process. We will consider
+        # that they can be ignored.
+
+        agb_importer.add_unlinked_flows_to_biosphere_database()
+        agb_importer.add_unlinked_activities()
+        agb_importer.statistics()
+
+        add_simapro_categories(agb_importer)
+
+        agb_importer.write_database()
+    #
+    # # WFLDB
+    # del bw.databases['WFLDB']
+    if 'WFLDB' in bw.databases:
+        print("Database has already been imported.")
+    else:
+        wfldb_csv_filepath = r"../databases/WFLDB_no_param.CSV"
+
+        wfldb_importer = bw.SimaProCSVImporter(wfldb_csv_filepath, "WFLDB")
+
+        wfldb_technosphere_migration = bw.Migration("wfldb-technosphere")
+        wfldb_technosphere_migration.write(
+            wfldb_technosphere_migration_data,
+            description="Specific technosphere fixes for World Food DB"
+        )
+
+        wfldb_importer.apply_strategies()
+        wfldb_importer.statistics()
+        wfldb_importer.migrate("wfldb-technosphere")
+        wfldb_importer.apply_strategies()
+        wfldb_importer.statistics()
+
+        # The only unlinked process is Wastewater, average {CH}| treatment of, capacity 5E9l/year | Cut-off, S - Copied from ecoinvent
+        # But it is only used by an orphan process, so it is considered ok to ignore it.
+
+        wfldb_importer.add_unlinked_activities()
+        wfldb_importer.add_unlinked_flows_to_biosphere_database()
+        wfldb_importer.statistics()
+
+        # Fixing an invalid uncertainty value for 3 exchanges
+        exchanges_to_fix = [
+            ('Coffee, regular, freeze dried, at plant (WFLDB)',
+             'Transformation into freeze-dried soluble coffee, green coffee, per kg product (WFLDB)'),
+            ('Coffee, regular, spray dried, at plant (WFLDB)',
+             'Transformation into spray-dried soluble coffee, green coffee, per kg product (WFLDB)'),
+            ('Coffee, regular, roast and ground, at plant (WFLDB)',
+             'Roasting and grinding, green coffee (WFLDB)')
+        ]
+
+        for activity_name, input_name in exchanges_to_fix:
+            exchange = next(y for x in wfldb_importer for y in x['exchanges']
+                            if (x['name'] == activity_name)
+                            and (y['name'] == input_name))
+            exchange['uncertainty type'] = 0
+            del exchange['loc']
+            del exchange['scale']
+            del exchange['negative']
+
+        add_simapro_categories(wfldb_importer)
+
+        wfldb_importer.write_database()
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,86 @@
+import copy
+
+import brightway2 as bw
+from bw2io.units import UNITS_NORMALIZATION, DEFAULT_UNITS_CONVERSION
+
+# Add here missing unit conversion in the form of:
+# (from_unit, to_unit, conversion_factor)
+ADDITIONAL_UNIT_CONVERSION = [
+    ('galons(US liq)', 'cubic meter', 0.003785412),
+    ('litre', 'cubic meter', 1e-3)  # I don't know why but this one is commented in BW source code
+]
+
+unit_conversion = {(conversion[0], conversion[1]): conversion[2]
+                   for conversion in DEFAULT_UNITS_CONVERSION + ADDITIONAL_UNIT_CONVERSION}
+
+# Add here missing unit normalization in the form of:
+# unit: reference_unit
+ADDITIONAL_UNITS_NORMALIZATION = {
+    'kilogramkilometer': 'kilogram kilometer',
+    'tonkilometer': 'ton kilometer',
+    'megawatthour': 'megawatt hour',
+    'kilowatthour': 'kilowatt hour',
+    'liter': 'litre',
+    'tons': 'ton',
+}
+UNITS_NORMALIZATION.update(ADDITIONAL_UNITS_NORMALIZATION)
+
+VALID_UNITS = set([x for x in UNITS_NORMALIZATION.keys()]) \
+    .union([x for x in UNITS_NORMALIZATION.values()]) \
+    .union([x for x in ADDITIONAL_UNITS_NORMALIZATION.keys()]) \
+    .union([x for x in ADDITIONAL_UNITS_NORMALIZATION.values()]) \
+    .union([x[0] for x in DEFAULT_UNITS_CONVERSION]) \
+    .union([x[1] for x in DEFAULT_UNITS_CONVERSION]) \
+    .union([x[0] for x in ADDITIONAL_UNIT_CONVERSION]) \
+    .union([x[1] for x in ADDITIONAL_UNIT_CONVERSION])
+
+
+def normalize_units(unit: str) -> str:
+    """
+    Examples:
+        >>> normalize_units('km')
+        'kilometer'
+
+    """
+    return UNITS_NORMALIZATION.get(unit.lower(), unit.lower() if unit.lower() in UNITS_NORMALIZATION.values() else unit)
+
+
+def harmonize_exchange_units(activities: dict) -> dict:
+    """
+        Converts the exchanges units and amounts to fit to the default unit of the exchange.
+        Returns a copy of the dataset with changed units and amounts.
+
+        Args:
+            activities (dict): Dictionary containing activities to import in a bw2 database.
+    """
+
+    activities = copy.deepcopy(activities)
+
+    for activity in activities.values():
+        for exchange in activity['exchanges']:
+            if exchange['type'] == 'production':
+                continue
+
+            input_unit = exchange['unit']
+
+            # If the exchange is from the list of activities to import, check that the unit is valid
+            if exchange['input'] in activities.keys():
+                if normalize_units(exchange['unit']) not in VALID_UNITS:
+                    raise ValueError(f"Invalid unit \"{exchange['unit']}\" for exchange \"{exchange['input']}\"")
+                reference_unit = normalize_units(exchange['unit'])
+            # If the exchange is from a background process, get the reference from brightway
+            else:
+                reference_unit = bw.get_activity(exchange['input'])['unit']
+
+            if input_unit != reference_unit:
+                if normalize_units(input_unit) != normalize_units(reference_unit):
+                    try:
+                        exchange['amount'] = exchange['amount'] * unit_conversion[normalize_units(input_unit),
+                                                                                  normalize_units(reference_unit)]
+                    except KeyError:
+                        raise KeyError(f'Unit conversion missing: '
+                                       f'{normalize_units(input_unit)} -> {normalize_units(reference_unit)}')
+
+                exchange['unit'] = reference_unit
+
+    return activities
